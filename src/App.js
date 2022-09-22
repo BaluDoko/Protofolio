30558aee0f36dcd1c9de40f945563b2c893aacab
Your Name <you@example.com>
2022-09-22 05:42:19 +0530

    skills section initiated

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ function App() {
         />
       ))}
       <hr />
+
       <Skills />
     </div>
   );
--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -1,17 +1,50 @@
 import React from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+// import LinearProgress, {
+//   LinearProgressWithLabel,
+// } from "@mui/material/LinearProgress";
+
 import "./Skills.css";
 
-const Skills = () => {
+function Skills() {
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+  };
+
   return (
-    <div>
-      <h1 className="common skill-container" style={{ marginTop: "15px" }}>
-        Skills
-      </h1>
-      <div className="skill">
-        <div>hello</div>
+    <div className="skill">
+      <div className="container">
+        <div className="skill-bx wow zoomIn">
+          <h2>Skills</h2>
+          <Carousel
+            responsive={responsive}
+            infinite={true}
+            className="owl-carousel owl-theme skill-slider"
+          >
+            <div className="item">
+              <img src="" alt="Image" />
+              <h5>Web Development</h5>
+              {/* <LinearProgressWithLabel value={80} /> */}
+            </div>
+            <div className="item">
+              <img src="" alt="Image" />
+              <h5>Brand Identity</h5>
+            </div>
+            <div className="item">
+              <img src="" alt="Image" />
+              <h5>Brand Identity</h5>
+            </div>
+          </Carousel>
+        </div>
       </div>
+
+      <img className="background-image-left" src="" alt="Image" />
     </div>
   );
-};
+}
 
 export default Skills;
